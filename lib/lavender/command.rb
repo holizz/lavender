dca8b3b373ccde3f785290f0c9fdbafd7f7779ba
Tom Adams <tom@holizz.com>
2010-09-27 16:24:46 +0100

    Add help text.

--- lib/lavender/command.rb
+++ lib/lavender/command.rb
@@ -2,14 +2,30 @@ module Lavender
   class Command
     def self.run args
       case args.first
+      when 'help'
+        self.help
+      when nil
+        self.help
+
       when 'compile'
         static = Lavender::Static.new
         static.run
       when 'server'
         raise ArgumentError, "Not yet implemented"
+
       else
         raise ArgumentError, "Command not recognised"
       end
+    end
+
+    def self.help
+      puts <<END
+Usage: lavender command
+
+Commands:
+  help      This message
+  compile   Compiles the project in the current working directory
+END
     end
   end
 end
